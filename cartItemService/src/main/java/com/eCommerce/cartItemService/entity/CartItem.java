87b769efaacf9dda3e0af63a3aa9f3310917ac4c
Malladi Varma <2427911@cognizant.com>
2025-08-14 13:10:27 +0530

    Added cart service microservice

---
+++ cartItemService/src/main/java/com/eCommerce/cartItemService/entity/CartItem.java
@@ -0,0 +1,68 @@
+package com.eCommerce.cartItemService.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+    @JsonIgnore
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long productId;
+
+    @Min(1)
+    private Integer quantity;
+
+    @DecimalMin("0.0")
+    private BigDecimal totalPrice;
+
+    // Getters and Setters
+    public Long getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(Long cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
